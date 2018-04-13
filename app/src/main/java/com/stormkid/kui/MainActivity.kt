03427786d35736317aeffcb64ac2ee8368038796
ke_li <stokid@126.com>
2018-04-13 10:49:34 +0800

    首次提交

--- app/src/main/java/com/stormkid/kui/MainActivity.kt
+++ app/src/main/java/com/stormkid/kui/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.stormkid.kui
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
