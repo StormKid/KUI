788c7c523b4dbc0542f6bed967cbae0e9d23c045
ke_li <stokid@126.com>
2018-06-11 10:32:26 +0800

    研究DSL

--- app/src/main/java/com/stormkid/kui/MainActivity.kt
+++ app/src/main/java/com/stormkid/kui/MainActivity.kt
@@ -2,21 +2,23 @@ package com.stormkid.kui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.stormkid.kui_base.dimen.AutoHelper
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        val layout = frameLayout {
-//            verticalLayout {
-//                val name = editText()
-//                button("Say Hello") {
-//                    onClick { toast("Hello, ${name.text}!") }
-//                }
-//            }
-//
-//        }
-        AutoHelper.BuildAuto(this).caculateViewSize()
+        val layout = frameLayout {
+            verticalLayout {
+                val name = editText()
+                button("Say Hello") {
+                    onClick { toast("Hello, ${name.text}!") }
+                }
+                verticalPadding = 10
+            }
+
+        }
+//        AutoHelper.BuildAuto(this).caculateViewSize()
     }
 }
