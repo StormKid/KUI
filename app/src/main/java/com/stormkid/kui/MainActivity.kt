9b94dae15f19db003f0afa6f832385a4b31abad3
ke_li <stokid@126.com>
2018-09-25 15:01:14 +0800

    AS 3.2重配置

--- app/src/main/java/com/stormkid/kui/MainActivity.kt
+++ app/src/main/java/com/stormkid/kui/MainActivity.kt
@@ -2,23 +2,21 @@ package com.stormkid.kui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import org.jetbrains.anko.*
-import org.jetbrains.anko.sdk25.coroutines.onClick
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val layout = frameLayout {
-            verticalLayout {
-                val name = editText()
-                button("Say Hello") {
-                    onClick { toast("Hello, ${name.text}!") }
-                }
-                verticalPadding = 10
-            }
-
-        }
+//        frameLayout {
+//            verticalLayout {
+//                val name = editText()
+//                button("Say Hello") {
+//                    onClick { toast("Hello, ${name.text}!") }
+//                }
+//                verticalPadding = 10
+//            }
+//
+//        }
 //        AutoHelper.BuildAuto(this).caculateViewSize()
     }
 }
