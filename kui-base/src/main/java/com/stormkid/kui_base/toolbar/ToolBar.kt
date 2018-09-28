a93e39e5fc3c9c3f1fd106bf759dcd5f550f8bfd
ke_li <stokid@126.com>
2018-09-28 16:59:49 +0800

    做完toolBar

--- kui-base/src/main/java/com/stormkid/kui_base/toolbar/ToolBar.kt
+++ kui-base/src/main/java/com/stormkid/kui_base/toolbar/ToolBar.kt
@@ -3,7 +3,9 @@ package com.stormkid.kui_base.toolbar
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
+import android.text.TextUtils
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
@@ -24,48 +26,94 @@ class ToolBar : RelativeLayout {
         val a = context.obtainStyledAttributes(attributeSet,
                 R.styleable.ToolBar, defAttr, 0)
         val imageSize = a.getDimensionPixelSize(R.styleable.ToolBar_image_dimen, 40).toFloat()
-        val leftIcon = a.getResourceId(R.styleable.ToolBar_left_image_icon,0)
-        val rightIcon = a.getResourceId(R.styleable.ToolBar_right_image_icon,0)
+        val leftIcon = a.getResourceId(R.styleable.ToolBar_left_image_icon, 0)
+        val rightIcon = a.getResourceId(R.styleable.ToolBar_right_image_icon, 0)
         val title = a.getString(R.styleable.ToolBar_title_text)
-        val titleGravity = a.getInt(R.styleable.ToolBar_title_gravity,0)
-        val isNav = a.getBoolean(R.styleable.ToolBar_is_nav_bar,true)
-        initImageView(imageSize,leftIcon,rightIcon)
-        initTitle(title,titleGravity,imageSize)
-        if (isNav&&context is Activity) this.getChildAt(0).setOnClickListener { context.finish() }
+        val titleSize = a.getDimensionPixelOffset(R.styleable.ToolBar_title_size, 14).toFloat()
+        val titleGravity = a.getInt(R.styleable.ToolBar_title_gravity, 0)
+        val isNav = a.getBoolean(R.styleable.ToolBar_is_nav_bar, true)
+        val content = a.getString(R.styleable.ToolBar_content_text)
+        val contentSize = a.getDimensionPixelOffset(R.styleable.ToolBar_content_size, 12).toFloat()
+        initImageView(imageSize, leftIcon, rightIcon)
+        initTitle(title, titleGravity, imageSize, titleSize)
+        initContent(content, contentSize)
+        if (isNav && context is Activity) this.getChildAt(0).setOnClickListener { context.finish() }
 
     }
 
-    private fun initTitle(title: String, titleGravity: Int, imageSize: Float) {
-        val group = this
-        val titleView = TextView(context).apply {
-            text = title
-            layoutParams = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,RelativeLayout.LayoutParams.WRAP_CONTENT).apply {
+    private fun initContent(content: String?, contentSize: Float) {
+        if (TextUtils.isEmpty(content)) return
+        contentView = TextView(context).apply {
+            text = content
+            setTextSize(TypedValue.COMPLEX_UNIT_SP, contentSize)
+            layoutParams = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT).apply {
                 addRule(RelativeLayout.CENTER_VERTICAL)
-                when(titleGravity){
-                     0-> addRule(RelativeLayout.CENTER_IN_PARENT)
-                     1-> setMargins(ViewUtils.dip2px(context,imageSize)+5,0,0,0)
-                     2-> setMargins(0,0,ViewUtils.dip2px(context,imageSize)+5,0)
-                 }
+                addRule(RelativeLayout.ALIGN_PARENT_RIGHT)
+            }
+        }
+        this.addView(contentView)
+    }
+
+    private fun initTitle(title: String?, titleGravity: Int, imageSize: Float, titleSize: Float) {
+        if (TextUtils.isEmpty(title)) return
+        val group = this
+        titleView = TextView(context).apply {
+            text = title
+            setTextSize(TypedValue.COMPLEX_UNIT_SP, titleSize)
+            layoutParams = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT).apply {
+                addRule(RelativeLayout.CENTER_VERTICAL)
+                when (titleGravity) {
+                    0 -> addRule(RelativeLayout.CENTER_IN_PARENT)
+                    1 -> setMargins(ViewUtils.dip2px(context, imageSize) + 5, 0, 0, 0)
+                    2 -> {
+                        addRule(RelativeLayout.ALIGN_PARENT_RIGHT)
+                        setMargins(0, 0, ViewUtils.dip2px(context, imageSize) + 5, 0)
+                    }
+                }
             }
 
         }
         group.addView(titleView)
     }
 
-    private fun initImageView(imageSize: Float, leftIcon: Int, rightIcon: Int) {
-        val leftImageView = ImageView(context).apply {
+    private var leftIcon:ImageView? = null
+    private var rightIcon:ImageView? = null
+    private var titleView:TextView? = null
+    private var contentView:TextView? = null
+    fun setOnToolClickListener(listener: OnClickListener){
+        this.setOnClickListener(listener)
+    }
+
+    fun setLeftIconClickListener(listener: OnClickListener){
+        if (null!=leftIcon)leftIcon?.setOnClickListener(listener)
+    }
+
+    fun setRightIconClickListener(listener: OnClickListener){
+        if (null!=rightIcon)rightIcon?.setOnClickListener(listener)
+    }
+
+    fun setTitle(text:String){
+        if (null!=titleView&&!TextUtils.isEmpty(text))titleView?.text = text
+    }
+
+    fun setContent(text:String){
+        if (null!=contentView&&!TextUtils.isEmpty(text))contentView?.text = text
+    }
+
+    private fun initImageView(imageSize: Float, leftIconRes: Int, rightIconRes: Int) {
+        leftIcon = ImageView(context).apply {
             layoutParams = RelativeLayout.LayoutParams(ViewUtils.dip2px(context, imageSize), ViewUtils.dip2px(context, imageSize))
             scaleType = ImageView.ScaleType.CENTER_CROP
-            setImageResource(leftIcon)
+            setImageResource(leftIconRes)
         }
 
-        val rightImageView = ImageView(context).apply {
+        rightIcon = ImageView(context).apply {
             layoutParams = RelativeLayout.LayoutParams(ViewUtils.dip2px(context, imageSize), ViewUtils.dip2px(context, imageSize)).apply { addRule(RelativeLayout.ALIGN_PARENT_RIGHT) }
             scaleType = ImageView.ScaleType.CENTER_CROP
-            setImageResource(rightIcon)
+            setImageResource(rightIconRes)
         }
-        this.addView(leftImageView)
-        this.addView(rightImageView)
+        this.addView(leftIcon)
+        this.addView(rightIcon)
     }
 
 
