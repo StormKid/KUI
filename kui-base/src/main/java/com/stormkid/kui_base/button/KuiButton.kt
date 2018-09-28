089bc97d1874378ba05b24222eb926c4e73a3cec
ke_li <stokid@126.com>
2018-09-28 18:03:04 +0800

    修改

--- kui-base/src/main/java/com/stormkid/kui_base/button/KuiButton.kt
+++ kui-base/src/main/java/com/stormkid/kui_base/button/KuiButton.kt
@@ -1,9 +1,35 @@
 package com.stormkid.kui_base.button
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.widget.LinearLayout
+
 /**
 
 @author ke_li
 @date 2018/9/28
  */
-class KuiButton {
+class KuiButton : LinearLayout {
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attributeSet: AttributeSet?) : this(context, attributeSet, 0)
+    @SuppressLint("Recycle")
+    constructor(context: Context, attributeSet: AttributeSet?, defAttr: Int) : super(context, attributeSet, defAttr) {
+
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+    }
+
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+        super.onLayout(changed, l, t, r, b)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+    }
+
 }
---
+++ kui-base/src/main/java/com/stormkid/kui_base/card/KuiCard.kt
@@ -0,0 +1,9 @@
+package com.stormkid.kui_base.card
+
+/**
+
+@author ke_li
+@date 2018/9/28
+ */
+class KuiCard {
+}
