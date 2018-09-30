3ebc1d09a0e39f4c16cf6da9c4ab67a5a5ce69f5
ke_li <stokid@126.com>
2018-09-30 16:51:22 +0800

    更新button

---
+++ app/src/main/java/com/stormkid/kui/sample/button/ButtonActitivy.kt
@@ -0,0 +1,15 @@
+package com.stormkid.kui.sample.button
+
+import com.stormkid.kui.MainActivity
+
+/**
+button sample
+@author ke_li
+@date 2018/9/30
+ */
+class ButtonActitivy:MainActivity() {
+    override fun getLayoutId(): Int =0
+
+    override fun initView() {
+    }
+}
