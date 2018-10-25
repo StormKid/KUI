0129ce7b946559adb2492a6fc00c23aa6bf17a1f
ke_li <stokid@126.com>
2018-10-25 18:24:08 +0800

    规范注释

--- kui-base/src/main/java/com/stormkid/kui_base/input/KuiInput.kt
+++ kui-base/src/main/java/com/stormkid/kui_base/input/KuiInput.kt
@@ -1,9 +1,34 @@
 package com.stormkid.kui_base.input
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Rect
+import android.util.AttributeSet
+import android.widget.EditText
+
 /**
 input 文字
 @author ke_li
 @date 2018/10/19
  */
-class KuiInput {
+class KuiInput : EditText{
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attributeSet: AttributeSet?) : this(context, attributeSet, 0)
+    @SuppressLint("Recycle")
+    constructor(context: Context, attributeSet: AttributeSet?, defAttr: Int) : super(context, attributeSet, defAttr)
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+    }
+
+    override fun onFocusChanged(focused: Boolean, direction: Int, previouslyFocusedRect: Rect?) {
+        super.onFocusChanged(focused, direction, previouslyFocusedRect)
+
+    }
 }
