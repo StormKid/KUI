cf2bbf1d87bb38c41ff6d8b59c59fb355e761876
ke_li <stokid@126.com>
2018-12-06 16:12:57 +0800

    toast 处理

--- kui-base/src/main/java/com/stormkid/kui_base/toast/KuiToast.kt
+++ kui-base/src/main/java/com/stormkid/kui_base/toast/KuiToast.kt
@@ -2,10 +2,9 @@ package com.stormkid.kui_base.toast
 
 import android.content.Context
 import android.graphics.PixelFormat
+import android.view.Gravity
 import android.view.WindowManager
-import com.stormkid.kui_base.TransLateOrScaleAnimationModel
-import com.stormkid.kui_base.helper.AnimationCallback
-import com.stormkid.kui_base.helper.AnimationHelpers
+import android.view.animation.AnimationSet
 
 /**
 自定义弹出土司提示
@@ -14,27 +13,17 @@ import com.stormkid.kui_base.helper.AnimationHelpers
  */
 class KuiToast (private val context: Context){
 
-    private val kuiToastView: KuiToastView = KuiToastView(context)
+    private val kuiToastView = KuiToastView(context)
     private val layoutparams = WindowManager.LayoutParams().apply {
         format = PixelFormat.TRANSPARENT
         flags = WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS
+        windowAnimations = AnimationSet.ZORDER_TOP
+        gravity = Gravity.BOTTOM
     }
 
     fun showToast(){
         val manager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
-
-        val helper =  AnimationHelpers(object : AnimationCallback{
-            override fun onFinished() {
-                manager.removeViewImmediate(kuiToastView)
-            }
-
-            override fun onStarted() {
-                manager.addView(kuiToastView,layoutparams)
-            }
-
-        })
-         helper.scaleAnimation(TransLateOrScaleAnimationModel(0f,1f,0f,0f,5000,kuiToastView))
-
+        manager.addView(kuiToastView,layoutparams)
     }
 
 
--- kui-base/src/main/java/com/stormkid/kui_base/toast/KuiToastView.kt
+++ kui-base/src/main/java/com/stormkid/kui_base/toast/KuiToastView.kt
@@ -2,38 +2,34 @@ package com.stormkid.kui_base.toast
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Canvas
 import android.graphics.Color
 import android.util.AttributeSet
-import android.widget.LinearLayout
+import android.view.ViewGroup
+import android.view.animation.TranslateAnimation
+import android.widget.RelativeLayout
+import com.stormkid.kui_base.InitDrawable
+import com.stormkid.kui_base.dimen.DimenUtils
+import com.stormkid.kui_base.drawables.BgDrawable
+import com.stormkid.kui_base.toolbar.KuiToolBar
 
 /**
 
 @author ke_li
-@date 2018/12/4
+@date 2018/12/6
  */
-class KuiToastView:LinearLayout {
+class KuiToastView:RelativeLayout {
 
+    private val kuiview =  KuiToolBar(context)
     constructor(context: Context) : this(context, null)
     constructor(context: Context, attributeSet: AttributeSet?) : this(context, attributeSet, 0)
     @SuppressLint("Recycle")
     constructor(context: Context, attributeSet: AttributeSet?, defAttr: Int) : super(context, attributeSet, defAttr){
-        setBackgroundColor(Color.CYAN)
-    }
-
-
-    override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
-
-    }
-
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-    }
-
-    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
-        super.onLayout(changed, l, t, r, b)
+        kuiview.layoutParams = ViewGroup.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT).apply {
+            height = DimenUtils.dip2px(context,40f)
+        }
+        BgDrawable.instance.getRoundDrawable(InitDrawable(Color.LTGRAY,kuiview,0f))
+        kuiview.startAnimation(TranslateAnimation(0f,0f,-100f,100f).apply { duration = 2000 })
+        addView(kuiview)
 
     }
-
 }
