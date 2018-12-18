ebd536f4423a57a4591eeadb51c0664100061442
ke_li <stokid@126.com>
2018-12-18 15:47:29 +0800

    card前准备

---
+++ kui-base/src/main/java/com/stormkid/kui_base/drawables/CardShapeDrawable.kt
@@ -0,0 +1,11 @@
+package com.stormkid.kui_base.drawables
+
+import android.graphics.drawable.ShapeDrawable
+
+/**
+
+@author ke_li
+@date 2018/12/18
+ */
+class CardShapeDrawable:ShapeDrawable() {
+}
